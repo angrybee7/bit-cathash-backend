0f2ba768e2d969c0af741f5c6fa373f6d954deda
angrybee7 <angrybee.contact@gmail.com>
2025-05-12 16:37:00 -0400

    update backend hosting code

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const serverless = require('serverless-http');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -233,6 +233,8 @@ app.get('/api/social-data', async (req, res) => {
   }
 }); 
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
+
+module.exports.handler = serverless(app);
